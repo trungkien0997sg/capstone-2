353242bf521818e55b8910921ca4ec714451d915
trungkien0997sg <works.trungkien0997@gmail.com>
2023-11-14 00:24:37 +0700

    goi api products (final)

--- controller.js
+++ controller.js
@@ -10,7 +10,12 @@ function renderProductList(productArr) {
           />
         </div>
         <div class="card__body">
-          <p>Hello</p>
+          <p class="font-bold">Name: ${data.name}</p>
+          <p class="pb-4"><span class="font-bold">Price: ${data.price} $</span></p>
+          <p class=""><span class="font-bold">Screen type:</span> ${data.screen}</p>
+          <p class=""><span class="font-bold">Back Camera:</span> ${data.backCamera} </p>
+          <p class=""><span class="font-bold">Front Camera:</span> ${data.frontCamera} </p>
+          <p class=""><span class="font-bold">Description:</span> ${data.desc}</p>
         </div>
       </div>
     `;
