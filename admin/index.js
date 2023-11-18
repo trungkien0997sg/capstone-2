74e9bd6363c62609ee091ed3f9ecf44535b1b533
trungkien0997sg <works.trungkien0997@gmail.com>
2023-11-18 21:55:30 +0700

    them update function

--- admin/index.js
+++ admin/index.js
@@ -1,3 +1,5 @@
+let idEdit = null;
+
 function fetchProductList() {
   axios({
     url: "https://653122ed4d4c2e3f333c7251.mockapi.io/products",
@@ -33,7 +35,7 @@ function deleteProduct(id) {
 }
 
 function createProduct() {
-  var productInfo = getData();
+  let productInfo = getData();
   axios({
     url: `https://653122ed4d4c2e3f333c7251.mockapi.io/products`,
     method: "POST",
@@ -51,4 +53,46 @@ function createProduct() {
     .catch(function (err) {
       console.log(err);
     });
+}
+
+function editProduct(id) {
+  idEdit = id;
+  axios({
+    url: `https://653122ed4d4c2e3f333c7251.mockapi.io/products/${id}`,
+    method: "GET",
+  })
+    .then(function (res) {
+      let product = res.data;
+      document.getElementById("TenSP").value = product.name;
+      document.getElementById("GiaSP").value = product.price;
+      document.getElementById("imgSP").value = product.img;
+      document.getElementById("MoTaSP").value = product.desc;
+      document.getElementById("ScreenSP").value = product.screen;
+      document.getElementById("bCamera").value = product.backCamera;
+      document.getElementById("fCamera").value = product.frontCamera;
+      document.getElementById("loaiSP").value = product.type;
+    })
+    .catch(function (err) {
+      console.log(err);
+    });
+}
+
+function updateProduct() {
+  let product = getData();
+  //gọi api cập nhật
+  axios({
+    url: `https://653122ed4d4c2e3f333c7251.mockapi.io/products/${idEdit}`,
+    method: "PUT",
+    data: product,
+  })
+    .then(function (res) {
+      fetchProductList();
+      Swal.fire({
+        title: "Thành công",
+        text: "Đã thêm sản phẩm",
+        icon: "success",
+        timer: 1500,
+      });
+    })
+    .catch(function (err) {});
 }
