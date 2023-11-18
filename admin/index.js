0a7dfb5e6ce97b5595e03fb81ab4bf795405cde2
trungkien0997sg <works.trungkien0997@gmail.com>
2023-11-18 19:23:46 +0700

    them nut delete

--- admin/index.js
+++ admin/index.js
@@ -12,3 +12,16 @@ function fetchProductList() {
 }
 
 fetchProductList();
+
+function deleteProduct(id) {
+  axios({
+    url: `https://653122ed4d4c2e3f333c7251.mockapi.io/products/${id}`,
+    method: "DELETE",
+  })
+    .then(function (res) {
+      fetchProductList();
+    })
+    .catch(function (err) {
+      console.log(err);
+    });
+}
